5dfc3800c64fd57cd24e98595f4d1029c5a16dc7
Stan Smith <choicedesign@gmail.com>
2017-07-19 15:22:03 -0600

    Install Jest, Material-UI and Tachyons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  CardTitle,
+  CardText
+} from "material-ui/Card";
+import FlatButton from "material-ui/FlatButton";
+import injectTapEventPlugin from "react-tap-event-plugin";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 class App extends Component {
   render() {
@@ -13,6 +28,35 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        <MuiThemeProvider>
+          <Card>
+            <CardHeader
+              title="URL Avatar"
+              subtitle="Subtitle"
+              avatar="https://robohash.org/guardiansrobots"
+            />
+            <CardMedia
+              overlay={
+                <CardTitle title="Overlay title" subtitle="Overlay subtitle" />
+              }
+            >
+              <img src="https://unsplash.it/800/?random" alt="" />
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              mattis pretium massa. Aliquam erat volutpat. Nulla facilisi. Donec
+              vulputate interdum sollicitudin. Nunc lacinia auctor quam sed
+              pellentesque. Aliquam dui mauris, mattis quis lacus id,
+              pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+              <FlatButton label="Action1" />
+              <FlatButton label="Action2" />
+            </CardActions>
+          </Card>
+        </MuiThemeProvider>
       </div>
     );
   }
